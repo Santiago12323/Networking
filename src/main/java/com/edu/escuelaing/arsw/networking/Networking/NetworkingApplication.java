7f02cfc2750d1b3f85f240ec6130475a6884cef3
santiago coronado <retrollaso@gmail.com>
2025-06-17 11:39:57 -0500

    first structure

---
+++ src/main/java/com/edu/escuelaing/arsw/networking/Networking/NetworkingApplication.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.arsw.networking.Networking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/escuelaing/arsw/networking/Networking/NetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.escuelaing.arsw.networking.Networking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
